45600d2e16f84a1ce13b2503b56c4894c40087f3
Dilip0552 <suthardilip.eis@gmail.com>
2025-07-19 00:08:42 +0530

    update API endpoint and CORS origins for production deployment

--- src/backend/main.py
+++ src/backend/main.py
@@ -26,7 +26,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://taskify-git-main-dilip0552s-projects.vercel.app/"
+    "https://taskify-git-main-dilip0552s-projects.vercel.app/",
+    "https://taskify-la02.onrender.com/"
 ]
 
 app.add_middleware(
